1568f084e4f7ba4b5a108d207d83a14580d22096
Abhishek2022 <avishekprasad26@gmail.com>
2019-06-28 18:16:44 +0530

    Synced with drive

---
+++ Maths/Combinatorics/nCr.cpp
@@ -0,0 +1,27 @@
+//Calculating nCr
+
+//--Using the identity nCr = (n-1)C(r-1) + (n-1)Cr.
+//--Storing the previous calculated values of nCr.
+//--This is faster when we need to calculate a large number of combinations, else using prod((i+r)/i) would be faster.
+	
+#include <bits/stdc++.h>
+using namespace std;
+
+long long c[1000][1000];
+int comb(int n, int r)
+{
+    if(r==0 || n==r)
+    {
+        c[n][r] = 1;
+        return 1;
+    }
+    else
+    {
+        if(c[n-1][r-1]==0)
+            c[n-1][r-1] = c[n-1][n-r] = comb(n-1,r-1)%1000000007;
+        
+        if(c[n-1][r]==0)
+            c[n-1][r] = c[n-1][n-r-1] = comb(n-1,r)%1000000007;
+    }
+    return (c[n-1][r] + c[n-1][r-1])%1000000007;
+}
---
+++ Maths/Modular Algebra/binary_power.cpp
@@ -0,0 +1,15 @@
+//BINARY-POWER (pow(a,b)%(m) in log(b))
+
+int binPow(long long a, long long b, long long m) 
+{   
+    a %= m;
+    long long res = 1;
+    while (b > 0) 
+    {
+        if (b & 1)
+           res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    } 
+    return res;
+}
